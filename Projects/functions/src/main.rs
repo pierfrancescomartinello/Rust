af30bc235c458e66e63690418e7955028ebea50d
Pierfrancesco Marinello <pierfrancescomartinello@outlook.it>
2022-08-22 23:38:43 +0200

    Functions Added

---
+++ Projects/functions/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+
+    let hi: i32 = 3;
+    let lol: char = 'h';
+    other_fucntion(hi, lol);
+}
+
+fn other_fucntion(x: i32, lol: char) {
+    println!("Hello to other {x} bitches. {lol}");
+}
